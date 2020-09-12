96fba01e3b6bc53bae690461c16041f410ea3df5
Jean-Simon Barry <jean.simon.barry@gmail.com>
2020-09-12 07:25:36 -0400

    initial rabbitmq setup

--- go.mod
+++ go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/streadway/amqp v1.0.0 // indirect
+	github.com/streadway/amqp v1.0.0
 	github.com/stretchr/testify v1.6.1
 	golang.org/x/sys v0.0.0-20200909081042-eff7692f9009 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
--- rabbitmq/rabbit_service.go
+++ rabbitmq/rabbit_service.go
@@ -1,1 +1,67 @@
 package rabbitmq
+
+import (
+	"github.com/google/uuid"
+	"github.com/streadway/amqp"
+	"log"
+)
+type RabbitInterface interface {
+	GetQueueName() string
+}
+var RabbitService RabbitInterface
+
+type rabbitService struct {
+	*amqp.Connection
+	queueName string
+}
+
+func (r *rabbitService) GetQueueName() string {
+	return r.queueName
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func init() {
+	var (
+		//TODO: read values from env
+		user = "guest"
+		host     = "localhost"
+		port     = "5672"
+		password = "guest"
+	)
+	conn, err := amqp.Dial("amqp://"+ user + ":" + password + "@" + host + ":" + port + "/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	queueName, _ := uuid.NewRandom()
+	_, err = ch.QueueDeclare(
+		queueName.String(), // name
+		false,              // durable
+		true,              // delete when unused
+		false,              // exclusive
+		false,              // no-wait
+		nil,                // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	//body := "Hello World!"
+	//err = ch.Publish(
+	//	"",     // exchange
+	//	q.Name, // routing key
+	//	false,  // mandatory
+	//	false,  // immediate
+	//	amqp.Publishing {
+	//		ContentType: "text/plain",
+	//		Body:        []byte(body),
+	//	})
+	failOnError(err, "Failed to publish a message")
+
+	RabbitService = &rabbitService{conn, queueName.String()}
+
+	defer ch.Close()
+}
